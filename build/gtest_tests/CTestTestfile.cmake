1507edf5c71cd1e7b1573871d47abeb5e283fe0b
gankere <annav7822@gmail.com>
2025-09-24 20:31:53 +0700

    Remove build artifacts: _CPack_Packages, version.h.in, build

--- build/gtest_tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/TPU_WorkSpace/3 term/lab2/gtest_tests
-# Build directory: D:/TPU_WorkSpace/3 term/lab2/build/gtest_tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/TPU_WorkSpace/3 term/lab2/build/gtest_tests/GTestTests[1]_include.cmake")
-subdirs("../_deps/googletest-build")
